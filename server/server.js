0480b6ad50e674b6cf5018955f162cd1089add5e
Your Name <you@example.com>
2021-10-27 00:23:01 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -8,29 +8,29 @@ servidor.on('connection', (socket)=> {
         
         console.log('mensaje recibido desde el cliente: ' + data )
         if(data == 'hola'){
-            let dia = new Date();
-            let hora = String (dia.getHours()).padStart(2,'0') + ':' + String(dia.getMinutes()).padStart(2,'0') + ':' + dia.getSeconds();
-            socket.write('Hola buenos dias\n' + fecha);
+          let dia = new Date();
+          let hora = String (dia.getHours()).padStart(2,'0') + ':' + String(dia.getMinutes()).padStart(2,'0') + ':' + dia.getSeconds();
+          socket.write('Hola buenos dias\n' + hora);
         }
         if(data == '¿como estas?'){
             socket.write('muy bien, gracias a Dios\n')
         }
         if(data == '¿como vas en la escuela?'){
-            socket.write('hasta el momento bien\n')
+        socket.write('hasta el momento bien\n')
         }        
         if(data == 'familia'){
             socket.write('todos se encuentran bien\n')
         }
         if(data == 'y tu trabajo'){
-            socket.write('muy pesado\n')
-        }
-        if(data == 'adios'){
-            socket.write('hasta luego')
+           socket.write('muy pesado\n')
+       }
+       if(data == 'adios'){
+           socket.write('hasta luego')
             socket.on('close',()=>{
                 console.log('comunicación finalizada' )
             })
-        }
-    })
+       }
+})
 
     //cierra el canal de comunicacion
    
